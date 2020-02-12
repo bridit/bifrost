13dd21bbef17791c97e690a42bf6d8ae7df4444b
Gustavo Siqueira <gus@brid-it.com>
2020-02-12 13:23:56 -0300

    Transformer

---
+++ src/Transformers/Transformer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bifrost\Transformers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Request;
+use League\Fractal\TransformerAbstract;
+
+class Transformer extends TransformerAbstract
+{
+
+  /**
+   * @var string
+   */
+  private static string $entityName;
+
+  protected function getResult(array $attributes, Model $entity)
+  {
+    $fields = array_filter(explode(',', Request::input('fields.customers')));
+
+    if (blank($fields)) {
+      return $attributes;
+    }
+
+    $fields = array_unique(array_merge($fields, [$entity->getKeyName()]));
+
+    return array_filter($attributes, fn($key) => in_array($key, $fields), ARRAY_FILTER_USE_KEY);
+  }
+
+}
