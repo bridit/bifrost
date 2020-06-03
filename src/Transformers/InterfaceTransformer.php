e3d99689de3528a488ece0f6873233751251cab1
Gustavo Siqueira <gus@brid-it.com>
2020-06-03 09:20:30 -0300

    feat: InterfaceTransformer fields filtering

--- src/Transformers/InterfaceTransformer.php
+++ src/Transformers/InterfaceTransformer.php
@@ -2,9 +2,26 @@
 
 namespace Bifrost\Transformers;
 
+use Illuminate\Support\Arr;
 use League\Fractal\TransformerAbstract;
 
 abstract class InterfaceTransformer extends TransformerAbstract
 {
+
+  /**
+   * @param string $entityName
+   * @param array $attributes
+   * @return array
+   */
+  protected function filter(string $entityName, array $attributes = [])
+  {
+    $fields = data_get(request()->get('fields'), $entityName);
+
+    if (blank($fields)) {
+      return $attributes;
+    }
+
+    return Arr::only($attributes, explode(',', $fields));
+  }
 
 }
