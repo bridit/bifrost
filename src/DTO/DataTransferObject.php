f9a8cc7957e0c67cc998492f1f1b1b99c719bd35
Gustavo Siqueira <gus@brid-it.com>
2020-06-07 22:50:59 -0300

    feat: ConvertibleFromArray

--- src/DTO/DataTransferObject.php
+++ src/DTO/DataTransferObject.php
@@ -2,15 +2,13 @@
 
 namespace Bifrost\DTO;
 
-use Carbon\Carbon;
-use ReflectionClass;
-use ReflectionProperty;
-use ReflectionException;
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Bifrost\Support\Concerns\ConvertibleFromArray;
 
 class DataTransferObject
 {
+
+  use ConvertibleFromArray;
 
   /**
    * @var array|null
@@ -23,29 +21,8 @@ class DataTransferObject
    */
   public function __construct(array $parameters = [])
   {
-    $class = new ReflectionClass(static::class);
-    $defaultProperties = $class->getDefaultProperties();
-
-    foreach ($class->getProperties(ReflectionProperty::IS_PUBLIC) as $reflectionProperty){
-      $property = $reflectionProperty->getName();
-      $value = data_get($parameters, Str::snake($property));
-      $default = data_get($defaultProperties, Str::camel($property));
-      $setter = 'set' . ucfirst($reflectionProperty->getName());
-
-      if ($class->hasMethod($setter)) {
-        $this->$setter($value);
-        continue;
-      }
-
-      if ($reflectionProperty->getType()->getName() === 'Carbon\Carbon' && !blank($value ?? $default)) {
-        $this->{Str::camel($property)} = Carbon::parse($value ?? $default);
-        continue;
-      }
-
-      $this->{Str::camel($property)} = $value ?? $default;
-    }
-
     $this->requestData = $parameters;
+    $this->fillFromArray($parameters);
   }
 
   /**
@@ -60,7 +37,6 @@ class DataTransferObject
   /**
    * @param Request $request
    * @return static
-   * @throws ReflectionException
    */
   public static function fromRequest(Request $request): self
   {
@@ -70,7 +46,6 @@ class DataTransferObject
   /**
    * @param array $params
    * @return static
-   * @throws ReflectionException
    */
   public static function fromArray(array $params): self
   {
---
+++ src/Support/Concerns/ConvertibleFromArray.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bifrost\Support\Concerns;
+
+use Carbon\Carbon;
+use ReflectionClass;
+use ReflectionProperty;
+use Illuminate\Support\Str;
+
+trait ConvertibleFromArray
+{
+
+  /**
+   * Fill object attributes from given associative array
+   * @param array $parameters
+   */
+  protected function fillFromArray(array $parameters = [])
+  {
+    $class = new ReflectionClass(static::class);
+    $defaultProperties = $class->getDefaultProperties();
+
+    foreach ($class->getProperties(ReflectionProperty::IS_PUBLIC) as $reflectionProperty){
+      $property = $reflectionProperty->getName();
+      $value = data_get($parameters, Str::snake($property));
+      $default = data_get($defaultProperties, Str::camel($property));
+      $setter = 'set' . ucfirst($reflectionProperty->getName());
+
+      if ($class->hasMethod($setter)) {
+        $this->$setter($value);
+        continue;
+      }
+
+      if ($reflectionProperty->getType()->getName() === 'Carbon\Carbon' && !blank($value ?? $default)) {
+        $this->{Str::camel($property)} = Carbon::parse($value ?? $default);
+        continue;
+      }
+
+      $this->{Str::camel($property)} = $value ?? $default;
+    }
+  }
+
+}
