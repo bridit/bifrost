eb0a76f2c3add6500122a7b1e59bb101e7431e00
Gustavo Siqueira <gus@brid-it.com>
2020-06-09 11:48:12 -0300

    fix: ApplicationTransformer indentation

--- src/Transformers/ApplicationTransformer.php
+++ src/Transformers/ApplicationTransformer.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Bifrost\Transformers;
+
 use Bifrost\Entities\Model;
 use Illuminate\Support\Str;
 use Bifrost\DTO\DataTransferObject;
@@ -8,24 +9,24 @@ use Illuminate\Support\Facades\Request;
 
 abstract class ApplicationTransformer
 {
-    public ?array $updateAllowed = [];
+  public ?array $updateAllowed = [];
 
-    public abstract function toModel(DataTransferObject $dto): Model;
+  public abstract function toModel(DataTransferObject $dto): Model;
 
-    public function prepareForUpdate(Model &$model, DataTransferObject $dto)
-    {
-      foreach($this->updateAllowed as $property){
-        call_if($dto->filled($property), fn() => $model->{Str::snake($property)} = $dto->{Str::camel($property)});
-      }
+  public function prepareForUpdate(Model &$model, DataTransferObject $dto)
+  {
+    foreach ($this->updateAllowed as $property) {
+      call_if($dto->filled($property), fn() => $model->{Str::snake($property)} = $dto->{Str::camel($property)});
     }
+  }
 
-    protected function getResult(array $attributes, Model $entity)
-    {
-      $fields = array_filter(explode(',', Request::input('fields.' . $entity->getTable())));
-      if (blank($fields)) {
-        return $attributes;
-      }
-      $fields = array_unique(array_merge($fields, [$entity->getKeyName()]));
-      return array_filter($attributes, fn($key) => in_array($key, $fields), ARRAY_FILTER_USE_KEY);
+  protected function getResult(array $attributes, Model $entity)
+  {
+    $fields = array_filter(explode(',', Request::input('fields.' . $entity->getTable())));
+    if (blank($fields)) {
+      return $attributes;
     }
+    $fields = array_unique(array_merge($fields, [$entity->getKeyName()]));
+    return array_filter($attributes, fn($key) => in_array($key, $fields), ARRAY_FILTER_USE_KEY);
+  }
 }
