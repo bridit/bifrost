3db5190082f9435e1a229930a31c74356b2d8039
Arthur Sander <arthur.sanderj@gmail.com>
2020-06-09 11:00:56 -0300

    fix ApplicationTransformer

--- helpers.php
+++ helpers.php
@@ -10,29 +10,3 @@ if (!function_exists('call_if')) {
   }
 
 }
-
-if(!function_exists('to_snake_case')){
-
-    function to_snake_case(string $input)
-    {
-        return strtolower( preg_replace(
-            ["/([A-Z]+)/", "/_([A-Z]+)([A-Z][a-z])/"], ["_$1", "_$1_$2"], lcfirst($input) ) );
-    }
-
-}
-
-if(!function_exists('to_camel_case')){
-
-    function to_camel_case(string $input, array $noStrip = []) {
-        // non-alpha and non-numeric characters become spaces
-        $input = preg_replace('/[^a-z0-9' . implode("", $noStrip) . ']+/i', ' ', $input);
-        $input = trim($input);
-        // uppercase the first character of each word
-        $input = ucwords($input);
-        $input = str_replace(" ", "", $input);
-        $input = lcfirst($input);
-
-        return $input;
-    }
-
-}
