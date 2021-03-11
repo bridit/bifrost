dce411736c87589c1e3549691df176ce629c232b
Gustavo Siqueira <gus@brid-it.com>
2021-03-11 13:30:10 -0300

    feat(InterfaceTransformer): ability to filter allowed attributes

--- src/Transformers/InterfaceTransformer.php
+++ src/Transformers/InterfaceTransformer.php
@@ -11,17 +11,24 @@ abstract class InterfaceTransformer extends TransformerAbstract
   /**
    * @param string $entityName
    * @param array $attributes
+   * @param array|null $allowed
    * @return array
    */
-  protected function filter(string $entityName, array $attributes = [])
+  protected function filter(string $entityName, array $attributes = [], ?array $allowed = null): array
   {
-    $fields = data_get(request()->get('fields'), $entityName);
+    $allowedAttributes = $allowed !== null
+      ? Arr::only($attributes, $allowed)
+      : $attributes;
 
-    if (blank($fields)) {
-      return $attributes;
+    $requestFields = data_get(request()->get('fields', []), $entityName);
+    $requestFields = array_filter(explode(',', $requestFields));
+
+    # Return all allowed attributes
+    if (blank($requestFields)) {
+      return $allowedAttributes;
     }
 
-    return Arr::only($attributes, explode(',', $fields));
+    return Arr::only($allowedAttributes, $requestFields);
   }
 
 }
